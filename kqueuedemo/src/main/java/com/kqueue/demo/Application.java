984e87b9545a62ed410fb1a605d56f96ba4cf784
Kumar Gaurav <kumargauravgupta3@gmail.com>
2018-03-24 15:05:59 +0530

    Added KQueueDemo project. It will help in writing test

---
+++ kqueuedemo/src/main/java/com/kqueue/demo/Application.java
@@ -0,0 +1,67 @@
+package com.kqueue.demo;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import com.kgcorner.kqueue.model.KQueue;
+import com.util.HttpUtil;
+import com.util.Response;
+import com.util.Strings;
+import com.util.exception.NetworkException;
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+    private static final String EVENT_SUBS_URL = "http://localhost:9000/queues/%s/events/%s/subscribe";
+    private static final String EVENT_RECV_URL = "http://localhost:8888/receive";
+    private static final String EVENT_DETAIL_URL = "http://localhost:8080/queues";
+    private static String queueId = null;
+    private static final Logger LOGGER = Logger.getLogger(Application.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+        try {
+            Response response = HttpUtil.sendGetRequest(EVENT_DETAIL_URL, null);
+            JsonElement jsonElement = new JsonParser().parse(response.getData());
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            queueId = jsonObject.get("id").getAsString();
+            subscribeEvent();
+        } catch (NetworkException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
+
+    private static void subscribeEvent() {
+        if(!Strings.isNullOrEmpty(queueId)) {
+            Map<String, String> body = new HashMap<>();
+            body.put("queueId", queueId);
+            body.put("tag", "feed-liked");
+            body.put("endpoint", EVENT_RECV_URL);
+            body.put("method", "POST");
+            body.put("content-type", "application/json");
+            String url = String.format(EVENT_SUBS_URL, queueId, "feed-liked");
+            try {
+                Response response = HttpUtil.sendPostRequest(url, null, HttpUtil.getPostDataString(body));
+                if(response.getStatus() == 200) {
+                    LOGGER.info("Event subscribed");
+                }
+            } catch (NetworkException e) {
+                LOGGER.error(e.getMessage(), e);
+            } catch (UnsupportedEncodingException e) {
+                LOGGER.error(e.getMessage(), e);
+            }
+        }
+        else{
+            LOGGER.error("Queue is not initialized");
+        }
+
+    }
+}
