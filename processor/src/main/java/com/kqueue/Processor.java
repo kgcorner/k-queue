0813296023f19a8b0ffca2e4578aaa20fffc84e3
Kumar Gaurav <kumargauravgupta3@gmail.com>
2018-03-26 08:27:29 +0530

    Added comment for process method

--- processor/src/main/java/com/kqueue/Processor.java
+++ processor/src/main/java/com/kqueue/Processor.java
@@ -3,5 +3,10 @@ package com.kqueue;
 import com.kqueue.exception.IncompatibleQueueException;
 
 public interface Processor extends Runnable{
+    /**
+     * Processes given queue accordign to their type
+     * @param queueId
+     * @throws IncompatibleQueueException
+     */
     void process(String queueId) throws IncompatibleQueueException;
 }
